517e0a8754f21da747aa2e8ef7e9c2e89aa65170
Saurabh Vijayvargia <saurabh.vijayvargia@oracle.com>
2024-07-11 14:02:28 +0530

    Push the code to fecth the files

---
+++ src/forex-rate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Define and Create output directory
+outputDirectory=../data/`date +%Y`/`date +%m`/`date +%d`
+mkdir -p $outputDirectory
+
+#Define OutputFile Name
+outputFile=$outputDirectory/sbi-forex-rate-card-`date +%Y%m%d`-`date +%H%M%S`.pdf
+
+#Download the file
+curl --location 'https://sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf' --output $outputFile
+
+#Do git commit
+#git add $outputFile
+#git commit "Sbi Forex Rate Card for `date +%Y%m%d`"
+#git push origin master
+
